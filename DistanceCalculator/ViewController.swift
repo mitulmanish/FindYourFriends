d7b897795bcc59e578227dd002655e79c69e2ef2
mitulmanish <mitul.manish@gmail.com>
2019-03-12 13:14:37 +1100

    Initial Commit

---
+++ DistanceCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DistanceCalculator
+//
+//  Created by Mitul Manish on 12/3/19.
+//  Copyright © 2019 Mitul Manish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
